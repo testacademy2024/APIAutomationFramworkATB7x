049fb5407fd43668738c1de6b4ce4162857721d5
testacademy2024 <sanketumrani14041992@gmail.com>
2026-03-07 23:08:36 +0530

    APIAutomationFramework-IntegrationTestCases

---
+++ src/test/java/com/thetestingacademy/base/BaseTest.java
@@ -0,0 +1,42 @@
+package com.thetestingacademy.base;
+
+import com.thetestingacademy.asserts.AssertActions;
+import com.thetestingacademy.endpoints.APIConstants;
+import com.thetestingacademy.modules.PayloadManager;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeTest;
+
+//Common to all Testcases like Father to all classes--Concept is single inheritance used
+public class BaseTest {
+
+    //  TC - Header
+    public RequestSpecification requestSpecification;
+    public AssertActions assertActions;
+    public PayloadManager payloadManager;
+    public JsonPath jsonPath;
+    public Response response;
+    public ValidatableResponse validatableResponse;
+
+    @BeforeTest
+    public void setUp() {
+        payloadManager =new PayloadManager();
+        assertActions=new AssertActions();
+        requestSpecification=new RequestSpecBuilder()
+                .setBaseUri(APIConstants.BASE_URL)
+                .addHeader("Content-Type","application/json")
+                .build().log().all();
+       //OR below way can be used
+
+        //        requestSpecification = RestAssured.
+//                given()
+//                .baseUri(APIConstants.BASE_URL)
+//                .contentType(ContentType.JSON)
+//                .log().all();
+
+
+    }
+}
--- src/test/java/com/thetestingacademy/base/BaseTests.java
+++
@@ -1,4 +0,0 @@
-package com.thetestingacademy.base;
-//Common to all Testcases like Father to all classes--Concept is single inheritance used
-public class BaseTests {
-}
